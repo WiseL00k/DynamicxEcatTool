9e403e586de310b41267d1e41c55bc2d2086feb8
WiseLook <1656438881@qq.com>
2026-07-09 02:54:06 +0800

    chore: ignore generated build artifacts

--- .qt/DynamicxEcatTool_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "DynamicxEcatTool")
-set(working_dir "D:/QtProjects/DynamicxEcatTool-codex")
-set(src_and_dest_list
-
-)
-set(timestamp_file "D:/QtProjects/DynamicxEcatTool-codex/.qt/DynamicxEcatTool_res.txt")
--- CMakeFiles/DynamicxEcatTool.dir/icons.i
+++
@@ -1,2 +0,0 @@
-#line 1 "D:\\QtProjects\\DynamicxEcatTool-codex\\icons.rc"
-IDI_ICON1 ICON DISCARDABLE "dynamicx_ecat_tool_logo.ico"
